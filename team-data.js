23b088fd4d999dec4de4f74dcf81d9bc92b111ff
Ichalkaranji <ich.mh.51@gmail.com>
2026-08-07 18:45:39 +0530

    Update team member: श्री. दिपक पंडीत

--- team-data.js
+++ team-data.js
@@ -6,7 +6,7 @@ const TEAM_MEMBERS = [
   },
   {
     "photo": "team-1786108094942-whatsapp-image-2026-08-07-at-6.35.24-pm.jpeg",
-    "name": "श्री.दिपक पंडीत"
+    "name": "श्री. दिपक पंडीत"
   },
   {
     "photo": "",
