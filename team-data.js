584bd7d996ac210caa27e5bcd7eaa08485f9df91
Ichalkaranji <ich.mh.51@gmail.com>
2026-08-07 18:44:52 +0530

    Reorder team member: श्री. नंदकुमार बांगड

--- team-data.js
+++ team-data.js
@@ -1,12 +1,12 @@
 // Team / committee members — edited via admin.html "सदस्य" tab.
 const TEAM_MEMBERS = [
   {
-    "photo": "team-1786108094942-whatsapp-image-2026-08-07-at-6.35.24-pm.jpeg",
-    "name": "श्री.दिपक पंडीत"
-  },
-  {
     "photo": "team-1786108355741-img-20260807-wa0095.jpg",
     "name": "श्री. नंदकुमार बांगड"
+  },
+  {
+    "photo": "team-1786108094942-whatsapp-image-2026-08-07-at-6.35.24-pm.jpeg",
+    "name": "श्री.दिपक पंडीत"
   },
   {
     "photo": "",
