66b669eec66fa661b83a27b0607c7adfde4d2c9f
Ichalkaranji <ich.mh.51@gmail.com>
2026-08-07 16:03:11 +0530

    Reorder team member: दिपक पंडीत

--- team-data.js
+++ team-data.js
@@ -1,11 +1,11 @@
 // Team / committee members — edited via admin.html "सदस्य" tab.
 const TEAM_MEMBERS = [
   {
-    "photo": "",
-    "name": "Vicky Lokare"
-  },
-  {
     "photo": "team-1786097342956-img-20260807-wa0069.jpg",
     "name": "दिपक पंडीत"
+  },
+  {
+    "photo": "",
+    "name": "Vicky Lokare"
   }
 ];
